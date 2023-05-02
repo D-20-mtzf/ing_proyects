2be084084bcbd030d0d64cf91fdc852eb5100de0
D-20-mtzf <118419023+D-20-mtzf@users.noreply.github.com>
2023-05-02 11:58:09 -0500

    Comentarios del programa

--- GameConfig.java
+++ GameConfig.java
@@ -6,29 +6,43 @@ package com.mycompany.softpong;
 
 /**
  *
- * @author EIT
+ * @author Daniel Martinez y Miguel Parada 
  */
+/**
+GameConfig es la clase que contiene la configuración del juego.
+La clase tiene tres variables miembro privadas: musicVolume, soundVolume y background, 
+que se utilizan para almacenar la configuración actual del juego.
+*/
 public class GameConfig {
     
         private static GameConfig instance;
     private int musicVolume;
     private int soundVolume;
     private String background;
-
+/**
+El constructor de la clase GameConfig es privado para evitar que se creen nuevas instancias de la clase. 
+En su lugar, utilizamos el método estático getInstance() para obtener la instancia única de la clase.
+*/
     private GameConfig() {
         // Constructor privado para evitar la creación de nuevas instancias.
         musicVolume = 50;
         soundVolume = 50;
         background = "default";
     }
-
+/**
+El método getInstance() comprueba si ya existe una instancia de la clase. 
+Si no existe, se crea una nueva instancia. Si ya existe, se devuelve la instancia existente.
+*/
     public static GameConfig getInstance() {
         if (instance == null) {
             instance = new GameConfig();
         }
         return instance;
     }
-
+/**
+Los métodos getMusicVolume(), setMusicVolume(), getSoundVolume(), setSoundVolume(), getBackground() y setBackground() 
+se utilizan para acceder y modificar la configuración del juego.
+*/
     public int getMusicVolume() {
         return musicVolume;
     }
